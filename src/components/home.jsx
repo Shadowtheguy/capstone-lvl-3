8ff9699aead68445b7ab0db9d3cbb0320012b51c
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-10-28 20:51:20 -0500

    Formatted the rest of the page, and added two more fonts

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,26 +6,28 @@ function Home() {
         <section className='container background-title'>
             <h1 className='pokefont-title text-center'>PokeMix!</h1>
         </section>
-        <section className='container background-description'>
+        <section className='container background-description text-white'>
             <h2 className='text-center'>Welcome to the website</h2>
             <p className='text-center'>Hello! Here you can mix and match all kind of Pokemon types, Abilities, and Stats to make your ideal pokemon, or even roll random combinations for fantasy fun! And if you have a good idea yourself, or want to see what other people submit, just go down below and make your own! Have fun!</p>
             <p className='text-center small opacity-50'>This website was made as a Capstone Project for my online classes.</p>
         </section>
-        <section>
+        <section className='container background-mixes'>
             <div className='row'>
                 <div className='col-5'>
-                    <h4>Pokemix 1</h4>
+                    <h4 className='pokefont-mixes text-center'>Pokemix 1</h4>
+                    <p>placeholder</p>
                 </div>
                 <div className='col-2'>
-                    <h2>VS</h2>
+                    <h2 className='pokefont-mixes text-center'>VS</h2>
                 </div>
                 <div className='col-5'>
-                    <h4>Pokemix 2</h4>
+                    <h4 className='pokefont-mixes text-center'>Pokemix 2</h4>
+                    <p>placeholder</p>
                 </div>
             </div>
         </section>
-        <section>
-            <h1>Make Your Own Here!</h1>
+        <section className='container background-submit'>
+            <h1 className='text-center'>Make Your Own Here!</h1>
             <p>Description leading to a form to fill out either on another page or as a pop up</p>
         </section>
         </>
