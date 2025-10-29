d6e423495a1c98edffd590d49b905bc3d6ee2a59
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-10-28 20:29:18 -0500

    Added and formatted Description for the website

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,9 +6,10 @@ function Home() {
         <section className='container background-title'>
             <h1 className='pokefont-title text-center'>PokeMix!</h1>
         </section>
-        <section>
-            <h2>Welcome to the website</h2>
-            <p>This is a description of the website and what you can do here</p>
+        <section className='container background-description'>
+            <h2 className='text-center'>Welcome to the website</h2>
+            <p className='text-center'>Hello! Here you can mix and match all kind of Pokemon types, Abilities, and Stats to make your ideal pokemon, or even roll random combinations for fantasy fun! And if you have a good idea yourself, or want to see what other people submit, just go down below and make your own! Have fun!</p>
+            <p className='text-center small opacity-50'>This website was made as a Capstone Project for my online classes.</p>
         </section>
         <section>
             <div className='row'>
